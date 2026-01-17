8d04927c8fcd55a2324e8fab1f6ea4a23c14fe3c
Pranav Shinde <shindepranav369@gmail.com>
2026-01-17 22:40:44 +0530

    voice model working perfect

--- backend/test.py
+++ backend/test.py
@@ -1,11 +1,3 @@
-from text_emotion import analyze_text
+from audio_emotion import analyze_audio
 
-tests = [
-    "I am extremely stressed and anxious.",
-    "I feel very happy today!",
-    "I feel lonely and sad.",
-    "This makes me angry."
-]
-
-for t in tests:
-    print(t, "=>", analyze_text(t))
+print(analyze_audio("sample.wav"))
